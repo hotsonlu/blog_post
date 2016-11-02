8fcfe08d5e2d5623136af80a611a3be92d6de5ee
Hotson lu <learning2010@gmail.com>
2016-11-02 18:28:02 +0800

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   resources :posts
 
   root 'posts#index'
+
+  get :sign_up, to: 'users#new'
+  post :sign_up, to: 'users#create'
+
+  get :sign_in, to: 'sessions#new'
+  post :sign_in, to: 'sessions#create'
+
+  get :sign_out, to: 'sessions#destroy'
 end
