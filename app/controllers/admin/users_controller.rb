c37e4064f6889df69186e6ea70308610e68d870e
Hotson lu <learning2010@gmail.com>
2016-11-02 23:30:30 +0800

    add show action Merge branch 'master' of github.com:hutsonlu/hutsonlu.github.io

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,7 +9,7 @@ class Admin::UsersController < Admin::BaseController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      redirect_to admin_root_path
+      redirect_to admin_posts_path
     else
       render 'new'
     end
