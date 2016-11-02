240e732b3004b0f60ea1da693a62c33844451906
Hotson lu <learning2010@gmail.com>
2016-11-02 19:06:27 +0800

    routes namespace

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,19 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.where(email: params[:user][:email]).first
+    if @user && @user.authenticate(params[:user][:password])
+      sign_in @user
+      redirect_to root_path
+    else
+      redirect_to sign_in_path
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,17 @@ Rails.application.routes.draw do
 
   root 'posts#index'
 
-  get :sign_up, to: 'users#new'
-  post :sign_up, to: 'users#create'
+  namespace :admin do
+    get :sign_up, to: 'users#new'
+    post :sign_up, to: 'users#create'
 
-  get :sign_in, to: 'sessions#new'
-  post :sign_in, to: 'sessions#create'
+    get :sign_in, to: 'sessions#new'
+    post :sign_in, to: 'sessions#create'
 
-  get :sign_out, to: 'sessions#destroy'
+    get :sign_out, to: 'sessions#destroy'
+
+    resources :posts
+    root to: 'posts#index'
+  end
+
 end
